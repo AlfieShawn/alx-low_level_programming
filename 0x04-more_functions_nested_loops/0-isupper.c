3eac84c7c1699fd69be81ced28d562ced477d73b
AlfieShawn <alfredshawn17@gmail.com>
2021-09-01 21:52:24 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<main.h>
+/**
+*main - Check for uppercase
+*
+*Return: Always 0 (Success)
+*/
+int _isupper(int c);
+{
+	char c;
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		Return(1);
+	}
+	else
+	{
+		Return(0);
+	}
+}
